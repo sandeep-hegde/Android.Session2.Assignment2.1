8d0d6ea61e44e96517a5a5300a2cd2e3b10a3d86
Sandeep Hegde <sandeep.hegde123@gmail.com>
2017-03-14 01:29:03 +0530

    Android Session 2 - Assignment 2.1
    Design a VIBGYOR screen using Linear Layout

---
+++ VIBGYOR/app/src/main/java/com/example/sandeep/vibgyor/MainActivity.java
@@ -0,0 +1,18 @@
+//Android Session 2 - Assignment 2.1
+
+//Design a VIBGYOR screen using Linear Layout
+
+
+package com.example.sandeep.vibgyor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newlinearlayout);
+    }
+}
